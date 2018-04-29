3864b13200934155b5623c00b4a37a59b75305a9
Sean Pollock <seanpollock3344@gmail.com>
2018-04-29 17:24:49 -0400

    estimating camera params. (no actual calibration for now)

--- VideoAnalyzer.py
+++ VideoAnalyzer.py
@@ -1,4 +1,5 @@
 import cv2
+import numpy as np
 
 class VideoAnalyzer():
     def __init__(self, face):
@@ -9,6 +10,10 @@ class VideoAnalyzer():
     def process_video(self, video_file):
         video = cv2.VideoCapture('./videos/{}'.format(video_file))
 
+        # "callibrate" camera
+        self.estimate_camera_calibration(video)
+
+        # run video through processor and display output
         while(True):
             ret, frame = video.read()
             if ret == True:
@@ -29,3 +34,19 @@ class VideoAnalyzer():
         # frame, closeup = self.face.get_face(frame, portrait)
         # frame, left_eye, right_eye = self.get_eyes(frame, closeup)
         return frame
+
+    ''' CALIBRATION '''
+
+    def estimate_camera_calibration(self, video):
+        ret, frame = video.read()
+        if ret == True:
+            frame_size = frame.shape
+            focal_length = frame_size[1]
+            center = (frame_size[1]/2, frame_size[0]/2)
+            self.camera_matrix = np.array(
+                [[focal_length, 0, center[0]],
+                [0, focal_length,  center[1]],
+                [0, 0, 1]],
+                dtype="double")
+        else:
+            raise ValueError('could not read frame from video')
